411ea9ebb39333365c8ec5d54848d751a9e1d654
Moses <mosesfat@gmail.com>
2024-01-09 18:12:09 +0800

    its working so far

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -3,7 +3,7 @@ export default function note() {
   return (
     <div className="w-[50%] mt-5">
         <h1 className="text-slate-500 text-center text-sm">Note: Video and audio content came from this 
-            <a href=""> youtube channel. </a>
+            <a className="hover:text-white duration-150 font-bold" href="https://www.youtube.com/@StingrayKaraoke" target="_blank" rel="noopener"> youtube channel. </a>
             I do not own any of the content being displayed and
             this is for entertainment purposes only.
         </h1>
--- src/pages/songBook.tsx
+++ src/pages/songBook.tsx
@@ -4,6 +4,10 @@ function songBook() {
   return (
     <div className="flex flex-col items-center text-white h-screen bg-slate-900">
       <Header text="Song Book" />
+      <div className="mt-10 text-xl text-center text-slate-500">
+        <h1>It's empty in here...</h1>
+        <h1>Feature not yet implemented. Stay tuned.</h1>
+      </div>
     </div>
   )
 }
